f96a2e9c0695ad3ab34feab4943c4ccffa4ee0f7
Andrius Janauskas <andrius.janauskas@gmail.com>
2015-12-22 13:15:51 +0200

    Initial commit

---
+++ lib/pronto/swiftlint.rb
@@ -0,0 +1,7 @@
+require "pronto/swiftlint/version"
+
+module Pronto
+  module Swiftlint
+    # Your code goes here...
+  end
+end
---
+++ lib/pronto/swiftlint/version.rb
@@ -0,0 +1,5 @@
+module Pronto
+  module Swiftlint
+    VERSION = "0.1.0"
+  end
+end
